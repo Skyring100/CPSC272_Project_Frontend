6177cf17813c4665e96b280bcf4f528c112fd49b
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2025-11-05 21:24:53 -0800

    You can now create an account in database through sign up page

--- src/app/signup.component.ts
+++ src/app/signup.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SignUp } from './models/signup.model';
+import { PollService } from './services/poll.services';
+import { Account } from './models/account.model';
 
 @Component({
   selector: 'app-poll',
@@ -11,9 +13,24 @@ import { SignUp } from './models/signup.model';
   styleUrls: ['./signup.component.css']
 })
 export class SignUpComponent {
-    
+  accountCreateSuccess : boolean | undefined = undefined;
+  usernameField : string | undefined;
+  passwordField : string | undefined;
+  confirmPasswordField : string | undefined;
+  constructor(private svc: PollService) {}
 
-    signUp(username: string, password:string){
-        //Query for the username and hashed password to retrieve a user object
-    }
+  signUp(){
+    const newAccount : Account = {
+      username : this.usernameField,
+      // NEED TO DO PROPER HASH. WE MIGHT NEED TO INSTALL 'bcrypt' LIBRARY TO DO THIS
+      password_hash : this.passwordField
+    };
+    this.svc.addAccount(newAccount).subscribe({
+      next: _ => this.accountCreateSuccess = true,
+      error: _ => {
+        this.accountCreateSuccess = false;
+        console.log("Error creating");
+      }
+    });
+  }
 }
