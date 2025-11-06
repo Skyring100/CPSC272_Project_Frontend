b508060e544d36ea59ced9722f99c2ab5a0de1e9
Pinecone <116664595+Pinecone-Programs@users.noreply.github.com>
2025-11-06 09:00:46 -0800

    Bunc of stuff

--- src/app/summary.component.ts
+++ src/app/summary.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { PollService } from './services/poll.services';
+import { Poll } from './models/poll.model';
+import { Account } from './models/account.model';
+import { PollWithOptions } from './models/pollWithOptions.model';
 
 @Component({
   selector: 'app-poll',
