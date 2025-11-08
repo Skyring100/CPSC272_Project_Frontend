c2b1dfe2d0596d3f1edcbfc7581600e94a5c08e1
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2025-11-08 13:20:27 -0800

    Started setting up frontend logic for account updating and deleteing

--- src/app/models/account.model.ts
+++ src/app/models/account.model.ts
@@ -1,6 +1,6 @@
 //A data type to interface with polls
 export interface Account {
-  uuid?: string;
+  uuid?: number;
   username?: string;
   password_hash?: string;
 }
--- src/app/summary.component.ts
+++ src/app/summary.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PollService } from './services/poll.services';
 import { Account } from './models/account.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-poll',
@@ -13,9 +14,39 @@ import { Account } from './models/account.model';
 })
 export class SummaryComponent {
   currentAccount : Account | undefined;
-  constructor(private svc: PollService) {}
+
+  showAccountEditOptions : boolean = false;
+  newUsernameField : string | undefined;
+  constructor(private svc: PollService, private router : Router) {}
   ngOnInit(){
     this.currentAccount = JSON.parse(localStorage.getItem('currentAccount') || '{}');
     console.log("Current account: "+this.currentAccount?.username + " with uuid "+this.currentAccount?.uuid);
+    if(this.currentAccount?.uuid == undefined){
+      // Take user to sign up page
+      this.router.navigateByUrl("/signup");
+    }
+  }
+
+  toggleShowAccountEditOptions(){
+    this.showAccountEditOptions = !this.showAccountEditOptions;
+  }
+
+  updateCurrentAccount(){
+    if(this.currentAccount?.uuid == undefined){
+      console.error("Current account has undefined UUID");
+    }else{
+      const updatedAccount : Account = {
+        // TODO: fill out updated account information
+      };
+      this.svc.updateAccount(this.currentAccount.uuid, updatedAccount);
+    }
+  }
+
+  deleteCurrentAccount(){
+    if(this.currentAccount?.uuid == undefined){
+      console.error("Current account has undefined UUID");
+    }else{
+      this.svc.deleteAccount(this.currentAccount.uuid);
+    }
   }
 }
