36ffd5f5da8addd5136b3631f5e21ef71dd255f8
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2025-11-08 15:19:13 -0800

    Added frontend logic for account updating and deleting

--- src/app/summary.component.ts
+++ src/app/summary.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { PollService } from './services/poll.services';
 import { Account } from './models/account.model';
 import { Router } from '@angular/router';
+import { Poll } from './models/poll.model';
+import { Option } from './models/option.model';
 
 @Component({
   selector: 'app-poll',
@@ -14,6 +16,7 @@ import { Router } from '@angular/router';
 })
 export class SummaryComponent {
   currentAccount : Account | undefined;
+  allUserPolls : Poll[] = [];
 
   showAccountEditOptions : boolean = false;
   newUsernameField : string | undefined;
@@ -24,6 +27,9 @@ export class SummaryComponent {
     if(this.currentAccount?.uuid == undefined){
       // Take user to sign up page
       this.router.navigateByUrl("/signup");
+    }else{
+      // Get all polls made by this user and get all get all votes from this user
+
     }
   }
 
@@ -36,9 +42,20 @@ export class SummaryComponent {
       console.error("Current account has undefined UUID");
     }else{
       const updatedAccount : Account = {
-        // TODO: fill out updated account information
+        uuid : this.currentAccount.uuid,
+        username : this.newUsernameField,
+        password_hash : this.currentAccount.password_hash
       };
-      this.svc.updateAccount(this.currentAccount.uuid, updatedAccount);
+      this.svc.updateAccount(this.currentAccount.uuid, updatedAccount).subscribe({
+        next: acc => {
+          this.currentAccount = acc;
+          localStorage.setItem('currentAccount', JSON.stringify(this.currentAccount));
+          console.log("Account Successfully Updated");
+        },
+        error: _ => {
+          console.error("Update failed");
+        }
+      });
     }
   }
 
@@ -46,7 +63,37 @@ export class SummaryComponent {
     if(this.currentAccount?.uuid == undefined){
       console.error("Current account has undefined UUID");
     }else{
-      this.svc.deleteAccount(this.currentAccount.uuid);
+      this.svc.deleteAccount(this.currentAccount.uuid).subscribe({
+        next: _ => {
+          this.currentAccount = {}
+          localStorage.clear();
+          console.log("Account Successfully Deleted");
+        },
+        error: _ => console.error("Delete failed")
+      });
+    }
+  }
+
+
+  select(selectedPoll: Poll, selectedOption: Option){
+    console.log("For the poll \""+selectedPoll.question+"\", you chose \""+selectedOption.content+"\"");
+    // Check if user is logged in
+    if(this.currentAccount?.uuid == undefined){
+      this.router.navigateByUrl('/signup');
+    }else{
+      // TODO: Send vote to database
+      console.log("Sending vote...");
+
+      // TODO: If vote was successful, display the results
+
+    }
+  }
+
+  deletePoll(p : Poll){
+    if(p.uuid == undefined){
+      console.error("Poll UUID is undefined");
+    }else{
+      this.svc.deletePoll(p.uuid);
     }
   }
 }
