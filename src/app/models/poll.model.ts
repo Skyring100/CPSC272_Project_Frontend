52bed234bf7aebd07395de1e56acc22e732582b4
Brandon <bwright@unbc.ca>
2025-11-18 22:21:48 -0800

    Removed some optional possiblities

--- src/app/models/poll.model.ts
+++ src/app/models/poll.model.ts
@@ -2,11 +2,11 @@ import { Option } from "./option.model";
 
 export interface Poll {
   poll_id: number;
-  question?: string;
-  options?: Option[];
+  question: string;
+  options: Option[];
   uuid?: number;
   username?: string;
-  is_visible?: boolean;
-  open_date?: string;
+  is_visible: boolean;
+  open_date: string;
   user_vote?: number | null;
 }
