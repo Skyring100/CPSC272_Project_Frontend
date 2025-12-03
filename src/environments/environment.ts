8844a7a730888ba9b1d41604a934d2f607185985
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2025-12-02 21:46:49 -0800

    Added /api to url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cpsc272-project-backend-516761d5e885.herokuapp.com/'
+  apiUrl: 'https://cpsc272-project-backend-516761d5e885.herokuapp.com/api'
 };
